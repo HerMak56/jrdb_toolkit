e542a0d4633ebcd85473444408bb99db45ca1848
HerMak56 <gdevcic10@gmail.com>
2026-02-12 12:57:30 +0300

    Fix for new versions numpy

--- tracking_eval/TrackEval/trackeval/datasets/__init__.py
+++ tracking_eval/TrackEval/trackeval/datasets/__init__.py
@@ -1,13 +1,16 @@
-from .kitti_2d_box import Kitti2DBox
-from .kitti_mots import KittiMOTS
-from .mot_challenge_2d_box import MotChallenge2DBox
-from .mots_challenge import MOTSChallenge
-from .bdd100k import BDD100K
-from .davis import DAVIS
-from .tao import TAO
-from .youtube_vis import YouTubeVIS
-from .head_tracking_challenge import HeadTrackingChallenge
-from .rob_mots import RobMOTS
+# Only import available JRDB datasets
 from .jrdb_2d_box import JRDB2DBox
 from .jrdb_3d_box import JRDB3DBox
+
+# Commented out - modules not available in this repo
+# from .kitti_2d_box import Kitti2DBox
+# from .kitti_mots import KittiMOTS
+# from .mot_challenge_2d_box import MotChallenge2DBox
+# from .mots_challenge import MOTSChallenge
+# from .bdd100k import BDD100K
+# from .davis import DAVIS
+# from .tao import TAO
+# from .youtube_vis import YouTubeVIS
+# from .head_tracking_challenge import HeadTrackingChallenge
+# from .rob_mots import RobMOTS
 
